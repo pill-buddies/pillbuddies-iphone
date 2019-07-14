73d2ee3d5457cce938bc99e56006066e60d44b2f
WilliamNHarvey <williamnharvey@gmail.com>
2019-07-14 15:33:14 -0700

    Initial Commit

---
+++ PillBuddies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PillBuddies
+//
+//  Created by William Harvey on 7/14/19.
+//  Copyright © 2019 Pill Buddies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
